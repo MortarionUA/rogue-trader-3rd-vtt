437a2dad26da59a6ff7bda8c969c3b8896485e23
Dmytro Shcherbakov <stalker19960919@gmail.com>
2025-07-12 16:17:28 +0300

    ship weapon init

--- src/module/rogue-trader-migrations.mjs
+++ src/module/rogue-trader-migrations.mjs
@@ -89,6 +89,7 @@ export async function checkAndMigrateWorld() {
                             'gear',
                             'tool',
                             'weapon',
+                            'shipWeapon',
                             'weaponModification',
                         ],
                     },
