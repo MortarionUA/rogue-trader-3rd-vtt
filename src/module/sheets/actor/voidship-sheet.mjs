8bda76336218f5848e78703071811569eab71062
Dmytro Shcherbakov <stalker19960919@gmail.com>
2025-07-13 21:06:41 +0300

    voidship sheet init

--- src/module/sheets/actor/voidship-sheet.mjs
+++ src/module/sheets/actor/voidship-sheet.mjs
@@ -1,6 +1,6 @@
-import { AcolyteSheet } from './acolyte-sheet.mjs';
+import { ActorContainerSheet } from './actor-container-sheet.mjs';
 
-export class VoidshipSheet extends AcolyteSheet {
+export class VoidshipSheet extends ActorContainerSheet {
     static get defaultOptions() {
         return foundry.utils.mergeObject(super.defaultOptions, {
             width: 1000,
@@ -13,4 +13,20 @@ export class VoidshipSheet extends AcolyteSheet {
     get template() {
         return `systems/rogue-trader-3rd/templates/actor/actor-voidship-sheet.hbs`;
     }
+
+    getData() {
+        const context = super.getData();
+        context.rt = CONFIG.rt;
+        const items = this.actor.items;
+        const sortedItems = Array.from(items).sort((a, b) => (a.sort ?? 0) - (b.sort ?? 0));
+        context.items = sortedItems;
+        return context;
+    }
+
+    async _onItemDamage(event) {
+        event.preventDefault();
+        const div = $(event.currentTarget);
+        game.rt.warn('Not Implemented for Vehicles Yet');
+    }
+
 }
