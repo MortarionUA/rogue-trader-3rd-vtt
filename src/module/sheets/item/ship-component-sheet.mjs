3581d1af4598c333bc2ef468723a1c55fdfb8eea
Dmytro Shcherbakov <stalker19960919@gmail.com>
2025-07-14 00:26:43 +0300

    voidship sheet init

--- src/module/sheets/item/ship-component-sheet.mjs
+++ src/module/sheets/item/ship-component-sheet.mjs
@@ -12,4 +12,20 @@ export class DarkHeresyShipComponentSheet extends DarkHeresyItemSheet {
     get template() {
         return `systems/rogue-trader-3rd/templates/item/item-ship-component-sheet.hbs`;
     }
+
+    activateListeners(html) {
+        super.activateListeners(html);
+
+        html.find("input[type='checkbox'][name^='items.']").on("change", async (event) => {
+            const input = event.currentTarget;
+            const itemId = input.name.split(".")[1];
+            const path = input.name.split(".").slice(2).join(".");
+            const checked = input.checked;
+
+            const item = this.actor.items.get(itemId);
+            if (!item) return;
+
+            await item.update({ [`system.${path.split("system.")[1]}`]: checked });
+        });
+    }
 }
