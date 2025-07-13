23c478d6cda2ec48f6689d23ce8f53289128066d
Dmytro Shcherbakov <stalker19960919@gmail.com>
2025-07-14 01:24:00 +0300

    voidship sheet init

---
+++ src/module/sheets/item/ship-trait-sheet.mjs
@@ -0,0 +1,15 @@
+import { DarkHeresyItemSheet } from './item-sheet.mjs';
+
+export class DarkHeresyShipTraitSheet extends DarkHeresyItemSheet {
+    static get defaultOptions() {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+            width: 650,
+            height: 500,
+            tabs: [{ navSelector: '.rt-navigation', contentSelector: '.rt-body', initial: 'stats' }],
+        });
+    }
+
+    get template() {
+        return `systems/rogue-trader-3rd/templates/item/item-trait-sheet.hbs`;
+    }
+}
